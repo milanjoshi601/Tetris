8f52c3d08aaff9a38d2a04f124f8522824aaf008
Milan <milanjoshi601@gmail.com>
2023-05-08 23:20:53 +0530

    Day 3 - Tetris Game

---
+++ Tetris/Assets/Scripts/Board.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public TetrominoData[] tetrominoData;
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+
+        for (int i = 0; i < this.tetrominoData.Length; i++)
+        {
+            this.tetrominoData[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnTile();
+    }
+
+    void SpawnTile()
+    {
+        int random = Random.Range(0, this.tetrominoData.Length);
+        TetrominoData data = this.tetrominoData[random];
+    }
+
+    public void Set(Tile tile)
+    {
+        for (int i = 0; i < tile.cells.Length; i++)
+        {
+            Vector3Int tilePos = tile.cells[i] + tile.pos;
+            this.tilemap.SetTile(tilePos, tile.data.tile);
+        }
+    }
+}
